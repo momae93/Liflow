6ffa734f77e013ade6a24d71a19207f855ca3060
Mickael AU <mickael.au@gmail.com>
2020-04-29 07:24:34 +0200

    chore(archi): refactored architecture folders for post feature

--- app/src/main/java/com/example/liflow/presentation/ui/post/fragment/NewPostFragment.kt
+++ app/src/main/java/com/example/liflow/presentation/ui/post/fragment/NewPostFragment.kt
@@ -1,4 +1,4 @@
-package com.example.liflow.presentation.ui.newPost
+package com.example.liflow.presentation.ui.post.fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.liflow.R
+import com.example.liflow.presentation.ui.post.viewmodel.NewPostViewModel
 
 class NewPostFragment : Fragment() {
 
--- app/src/main/java/com/example/liflow/presentation/ui/post/viewmodel/NewPostViewModel.kt
+++ app/src/main/java/com/example/liflow/presentation/ui/post/viewmodel/NewPostViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.liflow.presentation.ui.newPost
+package com.example.liflow.presentation.ui.post.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
